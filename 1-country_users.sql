0231f7da614fc92b3e7a8836e3f85221681873c3
amagyei <kwame.na@hotmail.com>
2024-10-17 19:10:34 +0000

    task 1

---
+++ 1-country_users.sql
@@ -0,0 +1,9 @@
+-- sql thingy for some things
+-- hello world 
+DROP TABLE IF EXISTS users;
+CREATE TABLE users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN')
+)
