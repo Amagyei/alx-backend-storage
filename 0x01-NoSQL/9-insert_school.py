76d7b6a6fb7a4ba405328b5e4283f84d583bc57d
amagyei <kwame.na@hotmail.com>
2024-10-22 12:27:48 +0000

    task 9

---
+++ 0x01-NoSQL/9-insert_school.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+'''Task 8's module.
+'''
+
+def list_all(mongo_collection):
+    '''Lists all documents in a collection.
+    '''
+    result = mongo_collection.insert_one(kwargs)
+    return result.inserted_id
